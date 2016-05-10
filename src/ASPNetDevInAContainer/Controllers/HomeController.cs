46e586ba7c4f2f4703ed97117d972f87638dd06c
Steve Lasker <StevenLasker@Hotmail.com>
2016-05-10 15:23:04 -0700

    Reset to RC2 2700

--- src/ASPNetDevInAContainer/Controllers/HomeController.cs
+++ src/ASPNetDevInAContainer/Controllers/HomeController.cs
@@ -2,20 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
-using Microsoft.AspNet.Mvc;
-using System.Text;
-using System.Collections;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNetDevInAContainer.Controllers
 {
     public class HomeController : Controller
     {
-        private IConfiguration _config;
-        public HomeController(IConfiguration config)
-        {
-            _config = config;
-        }
         public IActionResult Index()
         {
             return View();
@@ -23,23 +15,7 @@ namespace ASPNetDevInAContainer.Controllers
 
         public IActionResult About()
         {
-            //var value = _config.GetSection("Foo").GetSection("Bar").Value;
-            StringBuilder envVars = new StringBuilder();
-            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
-                envVars.Append((string)de.Key + ":" + (string)de.Value + "---" + Environment.NewLine);
-
-            ViewData["EnvVars"] = envVars.ToString();
-
-            ViewData["HostName"] = (Environment.GetEnvironmentVariables()["HOSTNAME"] != null) ?
-                Environment.GetEnvironmentVariables()["HOSTNAME"] :
-                Environment.GetEnvironmentVariables()["COMPUTERNAME"];
-
-            ViewData["OS"] = (Environment.GetEnvironmentVariables()["OS"] != null) ?
-                Environment.GetEnvironmentVariables()["OS"] :
-                Environment.GetEnvironmentVariables()["DNX_RUNTIME_ID"];
-
-            ViewData["PROCESSOR_ARCHITEW6432"] = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432");
-            ViewData["HOSTING_ENVIRONMENT"] = Environment.GetEnvironmentVariable("Hosting:Environment");
+            ViewData["Message"] = "Your application description page.";
 
             return View();
         }
