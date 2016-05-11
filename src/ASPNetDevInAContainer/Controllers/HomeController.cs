37b31653fa849bda836eae5aee7a823a64319e0c
Steve Lasker <StevenLasker@Hotmail.com>
2016-05-10 17:42:34 -0700

    Add docker support
    with some specific changes to show the platform differences

--- src/ASPNetDevInAContainer/Controllers/HomeController.cs
+++ src/ASPNetDevInAContainer/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Collections;
 
 namespace ASPNetDevInAContainer.Controllers
 {
@@ -12,9 +15,40 @@ namespace ASPNetDevInAContainer.Controllers
         {
             return View();
         }
-
         public IActionResult About()
         {
+            string hostName;
+            // Will only work on Windows
+            //hostName = Environment.GetEnvironmentVariable("COMPUTERNAME");
+
+            // Works on both Windows and Linux
+            hostName = Environment.GetEnvironmentVariable("COMPUTERNAME") ??
+                                Environment.GetEnvironmentVariable("HOSTNAME");
+
+            ViewData["HOSTNAME"] = hostName;
+
+            TimeZoneInfo tzInfo = null;
+            // Will only work on Windows
+            //tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+            // Works on both Windows and Linux
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                tzInfo = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            else
+                tzInfo = TimeZoneInfo.FindSystemTimeZoneById("America/New_York");
+
+            ViewData["TZINFO"] = tzInfo;
+
+            string hostingEnvironment = Environment.GetEnvironmentVariable("Hosting:Environment");
+            ViewData["HOSTING_ENVIRONMENT"] = hostingEnvironment;
+
+            StringBuilder envVars = new StringBuilder();
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                envVars.Append(string.Format("<strong>{0}</strong>:{1}<br \\>", de.Key, de.Value));
+
+            ViewData["ENV_VARS"] = envVars.ToString();
+
+
             ViewData["Message"] = "Your application description page.";
 
             return View();
